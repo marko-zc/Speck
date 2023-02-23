e9e23aed56cc688bfb45b88f1458b5239966779f
marko-zc <marko.zarkovic.cerneli@gmail.com>
2023-02-23 17:54:01 +0100

    Initial commit.

---
+++ script.js
@@ -0,0 +1,17 @@
+const techInput = document.getElementById("techInput");
+const techList = document.getElementById("techList");
+
+addTech = (e) => {
+    e.preventDefault();
+    const listElement = document.createElement("li");
+    listElement.innerText = techInput.value;
+    listElement.classList.add("techItem");
+    techList.appendChild(listElement);
+}
+
+clearTechs = () => {
+    const listElements = document.getElementsByClassName("techItem");
+    while (listElements.length > 0) {
+        listElements[0].remove();
+    }
+}
